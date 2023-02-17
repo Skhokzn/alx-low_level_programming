9d77273a25141299bbaa5f012d670a49f4fe6752
Skhokzn <smsikhokele9@gmail.com>
2023-02-16 23:50:56 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+* main - Program that print a line using printf function
+* Retrun: 0(on success, non-zero on error)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
