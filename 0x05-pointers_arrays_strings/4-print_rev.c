e4c60082e3949c96be2c4418640b2a4014555d76
Skhokzn <smsikhokele9@gmail.com>
2023-03-02 17:47:09 -0800

    0-strcat.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,16 @@
  * @s: string
  * return: 0
  */
+
 void print_rev(char *s)
+
 {
 	int longi = 0;
 	int 0;
 	while (*s != '\0')
+
 	{
+
 		longi++;
 		s++;
 
@@ -18,9 +22,12 @@ void print_rev(char *s)
 
 	s--;
 	for (0 = longi; 0 > 0; 0--)
+
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
+
 }
